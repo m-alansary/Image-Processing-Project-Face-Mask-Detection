11d74123dd0f8728da37ab12fabfd939b7c521a6
m-alansary <mohamedalansary74@gmail.com>
2021-05-25 03:37:33 +0200

    Refactoring

--- camera.py
+++ camera.py
@@ -1,8 +1,7 @@
 import csv
 import cv2
 import os
-
-currentId = 0
+from studentsData import add_student, currentId
 
 
 def test_and_detect():
@@ -13,16 +12,16 @@ def test_and_detect():
     cap = cv2.VideoCapture(0)
 
     while True:
-        _, img = cap.read()
+        _, image = cap.read()
 
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
         faces = cascadeFace.detectMultiScale(gray, 1.3, 5, minSize=(30, 30),flags = cv2.CASCADE_SCALE_IMAGE)
 
         for (p1, p2, p3, p4) in faces:
-            cv2.rectangle(img, (p1, p2), (p1 + p3, p2 + p4), (0, 0, 255), 2) # BGR
+            cv2.rectangle(image, (p1, p2), (p1 + p3, p2 + p4), (0, 0, 255), 2) # BGR
 
-        cv2.imshow('Test Camera', img)
+        cv2.imshow('Test Camera', image)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
@@ -31,26 +30,22 @@ def test_and_detect():
     cv2.destroyAllWindows()
 
 def capture_training_images(name):
-    global currentId
-    id = str(currentId)
-    currentId += 1
-
     if name.isalpha():
         cam = cv2.VideoCapture(0)
         detector = cv2.CascadeClassifier("haarcascade_default.xml")
         imageNo = 0
 
         while(True):
-            ret, img = cam.read()
-            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            ret, image = cam.read()
+            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
             faces = detector.detectMultiScale(gray, 1.3, 5, minSize=(30,30),flags = cv2.CASCADE_SCALE_IMAGE)
 
             for (p1, p2, p3, p4) in faces:
-                cv2.rectangle(img, (p1, p2), (p1 + p3, p2 + p4), (0, 0, 255), 2) # BGR
+                cv2.rectangle(image, (p1, p2), (p1 + p3, p2 + p4), (0, 0, 255), 2) # BGR
                 imageNo += 1
-                cv2.imwrite("Training Images" + os.sep + name + "." + id + '.' +
+                cv2.imwrite("Training Images" + os.sep + name + "." + str(currentId) + '.' +
                             str(imageNo) + ".jpg", gray[p2 : p2 + p4, p1 : p1 + p3])
-                cv2.imshow('Capturing Image', img)
+                cv2.imshow('Capturing Image', image)
             if cv2.waitKey(100) & 0xFF == ord('q'):
                 break
             elif imageNo > 100:
@@ -58,7 +53,9 @@ def capture_training_images(name):
         
         cam.release()
         cv2.destroyAllWindows()
-        res = "Training data is saved for ID: " + id + " and Name: " + name
-        row = [id, name]
+        res = "Training data is saved for ID: " + str(currentId) + " and Name: " + name
+        row = [currentId, name]
+        add_student(name)
+
         return row
 
